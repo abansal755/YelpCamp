ba14786eaec6fa08c09cd4bef6b09b5040d394dc
Akshit Bansal <akshit.bansal755@gmail.com>
2021-01-13 17:34:05 +0530

    handled delete request

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -61,7 +61,8 @@ router.get('/:id',middleware.findCampground,wrapAsync(async (req,res) => {
 router.post('/',middleware.ensureLogin,upload.array('image',10),wrapAsync(async (req,res) => {
     const campground = new Campground(req.body.campground);
     campground.author = req.user._id;
-    for(const file of req.files) campground.image.push(`/images/uploads/${req.user._id}/${file.filename}`);
+    for(const file of req.files) campground.image.push(`/images/uploads/${req.user._id}/${file.filename}`); 
+    if(req.files.length === 0) throw new AppError('Atleast 1 image is required',400);
     await campground.save();
     req.flash('success','Successfully created a campground');
     res.redirect(`/campgrounds/${campground._id}`);
@@ -74,16 +75,19 @@ router.get('/:id/edit',middleware.findCampground,middleware.ensureLogin,middlewa
 router.patch('/:id',middleware.findCampground,middleware.ensureLogin,middleware.authorizeCampground,upload.array('image',10),wrapAsync(async (req,res) => {
     const {id} = req.params;
     const {campground,include} = req.body;
+    const removeArray = [];
+    if(!include && req.files.length === 0) throw new AppError('Atleast 1 image is required',400);
+    if(req.files.length+include.length > 10) throw new AppError('Atmax 10 images are allowed',400);
     for(const key in campground) req.campgroundQuery[key] = campground[key];
-    const size = include?include.length:req.campgroundQuery.image.length;
-    for(let i=size-1;i>=0;i--){
-        if((include && !include[i]) || (!include)){
-            await fs.unlink(`public${req.campgroundQuery.image}`);
+    for(let i=req.campgroundQuery.image.length-1;i>=0;i--){
+        if((include && !include[i]) || !include){
+            removeArray.push(`public${req.campgroundQuery.image[i]}`);
             req.campgroundQuery.image.splice(i,1);
         }
     }
     for(const file of req.files) req.campgroundQuery.image.push(`/images/uploads/${req.user._id}/${file.filename}`);
     await req.campgroundQuery.save();
+    for(const url of removeArray) await fs.unlink(url);
     req.flash('success','Successfully updated the campground');
     res.redirect(`/campgrounds/${id}`);
 }));
@@ -93,7 +97,8 @@ router.delete('/:id',middleware.findCampground,middleware.ensureLogin,middleware
     const {id} = req.params;
     const campground = await Campground.findByIdAndDelete(id).exec();
     await Review.deleteMany({_id:{$in:campground.reviews}});
-    await fs.unlink(`public${req.campgroundQuery.image}`);
+    console.log(campground.image);
+    for(const file of campground.image) await fs.unlink(`public/${file}`);
     req.flash('success','Successfully deleted the campground');
     res.redirect('/campgrounds');
 }));
